215fb59ab56bb2fdd9ed5b0e7d328096a7a94f8d
clj198606061111@163.com <clj-1986>
2020-10-06 22:27:12 +0800

    响应打印时间

--- server1/src/main/java/com/itclj/server1/controller/S1Controller.java
+++ server1/src/main/java/com/itclj/server1/controller/S1Controller.java
@@ -1,16 +1,24 @@
 package com.itclj.server1.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @RestController
 @RequestMapping("/s1")
 public class S1Controller {
 
+    private Logger logger = LoggerFactory.getLogger(S1Controller.class);
+
     @GetMapping("/a")
-    public String s1(){
-        return "s1";
+    public String s1() {
+        logger.info("S1 request time={}", System.currentTimeMillis());
+        return "s1=" + LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME);
     }
 
 }
--- server2/src/main/java/com/itclj/server2/controller/S2Controller.java
+++ server2/src/main/java/com/itclj/server2/controller/S2Controller.java
@@ -1,15 +1,23 @@
 package com.itclj.server2.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @RestController
 @RequestMapping("/s2")
 public class S2Controller {
 
+    private Logger logger = LoggerFactory.getLogger(S2Controller.class);
+
     @GetMapping("/b")
     public String s2() {
-        return "s2";
+        logger.info("S2 request time={}", System.currentTimeMillis());
+        return "s2=" + LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME);
     }
 }
